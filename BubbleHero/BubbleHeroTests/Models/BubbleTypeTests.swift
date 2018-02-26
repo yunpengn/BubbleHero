a6b2c8ea363114b1df2c473e41129cb0991a7130
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-02-26 22:04:14 +0800

    Add some tests

---
+++ BubbleHero/BubbleHeroTests/Models/BubbleTypeTests.swift
@@ -0,0 +1,19 @@
+//
+//  BubbleTypeTests.swift
+//  BubbleHeroTests
+//
+//  Created by Yunpeng Niu on 26/02/18.
+//  Copyright © 2018年 Yunpeng Niu. All rights reserved.
+//
+
+import XCTest
+@testable import BubbleHero
+
+class BubbleTypeTests: XCTestCase {
+    func testNextColor() {
+        XCTAssertEqual(BubbleType.blue.nextColor(), BubbleType.green, "The next color of blue is wrong")
+        XCTAssertEqual(BubbleType.green.nextColor(), BubbleType.orange, "The next color of green is wrong")
+        XCTAssertEqual(BubbleType.orange.nextColor(), BubbleType.red, "The next color of orange is wrong")
+        XCTAssertEqual(BubbleType.red.nextColor(), BubbleType.blue, "The next color of red is wrong")
+    }
+}
---
+++ BubbleHero/BubbleHeroTests/Models/LevelTests.swift
@@ -0,0 +1,61 @@
+//
+//  LevelTests.swift
+//  BubbleHeroTests
+//
+//  Created by Yunpeng Niu on 26/02/18.
+//  Copyright © 2018 Yunpeng Niu. All rights reserved.
+//
+
+import XCTest
+@testable import BubbleHero
+
+class LevelTests: XCTestCase {
+    func testInit() {
+        let level = Level()
+        XCTAssertEqual(level.numOfRows, Settings.numOfRows, "The number of rows is wrong.")
+        XCTAssertEqual(level.evenCount, Settings.cellPerRow, "The even count is wrong.")
+        XCTAssertEqual(level.oddCount, Settings.cellPerRow - 1, "The odd count is wrong.")
+    }
+
+    func testAddOrUpdateBubble() {
+        let level = Level()
+        level.addOrUpdateBubble(BubbleType.blue, row: 0, column: 0)
+        XCTAssertEqual(level.getBubbleAt(row: 0, column: 0), BubbleType.blue,
+                       "The bubble is not inserted correctly.")
+
+        level.addOrUpdateBubble(BubbleType.green, row: 0, column: 0)
+        XCTAssertEqual(level.getBubbleAt(row: 0, column: 0), BubbleType.green,
+                       "The bubble is not updated correctly.")
+    }
+
+    func testHasBubble() {
+        let level = Level()
+        level.addOrUpdateBubble(BubbleType.blue, row: 0, column: 0)
+        XCTAssertTrue(level.hasBubbleAt(row: 0, column: 0),
+                      "hasBubble returns the wrong result.")
+        XCTAssertFalse(level.hasBubbleAt(row: 0, column: 1),
+                       "hasBubble returns the wrong result.")
+    }
+
+    func testGetBubbleAt() {
+        let level = Level()
+        level.addOrUpdateBubble(BubbleType.blue, row: 0, column: 0)
+        XCTAssertEqual(level.getBubbleAt(row: 0, column: 0), BubbleType.blue,
+                       "getBubbleAt is wrong.")
+    }
+
+    func testDeleteBubble() {
+        let level = Level()
+        level.addOrUpdateBubble(BubbleType.blue, row: 0, column: 0)
+        XCTAssertTrue(level.hasBubbleAt(row: 0, column: 0), "The bubble is not inserted correctly.")
+        level.addOrUpdateBubble(BubbleType.green, row: 0, column: 0)
+        XCTAssertTrue(level.hasBubbleAt(row: 0, column: 0), "deleteBubble is wrong.")
+        level.addOrUpdateBubble(BubbleType.blue, row: 0, column: 0)
+        XCTAssertFalse(level.hasBubbleAt(row: 0, column: 0), "deleteBubble is wrong.")
+
+        level.addOrUpdateBubble(BubbleType.blue, row: 1, column: 1)
+        XCTAssertTrue(level.hasBubbleAt(row: 1, column: 1), "The bubble is not inserted correctly.")
+        level.deleteBubbleAt(row: 1, column: 1)
+        XCTAssertFalse(level.hasBubbleAt(row: 1, column: 1), "deleteBubble is wrong.")
+    }
+}
