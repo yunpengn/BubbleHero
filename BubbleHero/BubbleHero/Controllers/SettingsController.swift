b5c1589ada8c98fcff30f03cbda5aee779dc0400
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-02-27 13:24:16 +0800

    Add settings view

---
+++ BubbleHero/BubbleHero/Controllers/SettingsController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsController.swift
+//  BubbleHero
+//
+//  Created by Yunpeng Niu on 27/02/18.
+//  Copyright © 2018 Yunpeng Niu. All rights reserved.
+//
+
+import UIKit
+
+/**
+ Controller for the settings view, in which the player can set some options.
+
+ - Author: Niu Yunpeng @ CS3217
+ - Date: Feb 2018
+ */
+class SettingsController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    /// Always hide the status bar on the top.
+    override var prefersStatusBarHidden: Bool {
+        return false
+    }
+}
