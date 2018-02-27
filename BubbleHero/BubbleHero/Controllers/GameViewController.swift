38e3ba9d452f025563c356dbc9aba1af0ecf5393
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-02-27 13:35:13 +0800

    Add game view

---
+++ BubbleHero/BubbleHero/Controllers/GameViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GameViewController.swift
+//  BubbleHero
+//
+//  Created by Yunpeng Niu on 27/02/18.
+//  Copyright © 2018 Yunpeng Niu. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
