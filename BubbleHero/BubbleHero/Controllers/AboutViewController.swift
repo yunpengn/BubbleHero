7cdbca2b05f648c0895aca7b1f2895a27a1d890d
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-02-27 21:40:32 +0800

    Add stingers

--- BubbleHero/BubbleHero/Controllers/AboutViewController.swift
+++ BubbleHero/BubbleHero/Controllers/AboutViewController.swift
@@ -15,9 +15,32 @@ import UIKit
  - Date: Feb 2018
  */
 class AboutViewController: UIViewController {
+    /// Some photos used as stingers.
+    private let stingers = [#imageLiteral(resourceName: "studio_brand"), #imageLiteral(resourceName: "cs3217_logo"), #imageLiteral(resourceName: "swift_logo")]
+    /// The stinger currently used.
+    private var currentStingerIndex = 0
+    /// The number of clicks on the current stinger.
+    private var clickCount = 0
+
     /// Always hide the status bar on the top.
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+
+    /// Changes the image when user clicks the brand logo for enough times.
+    /// - Parameter sender: The view being clicked.
+    @IBAction func brandLogoClicked(_ sender: UIButton) {
+        guard currentStingerIndex < stingers.count else {
+            return
+        }
+
+        if clickCount < currentStingerIndex * 3 {
+            clickCount += 1
+        } else {
+            sender.setImage(stingers[currentStingerIndex], for: .normal)
+            currentStingerIndex += 1
+            clickCount = 0
+        }
     }
 
     /// Goes back to the last view when back button is pressed.
